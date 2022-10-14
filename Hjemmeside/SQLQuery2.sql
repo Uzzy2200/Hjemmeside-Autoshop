f0acf4c583ac3e73cbe9e2b2038baa23b3039713
Uzzy98 <98802291+Uzzy2200@users.noreply.github.com>
2022-10-14 11:31:40 +0200

    Add files via upload

---
+++ Hjemmeside/SQLQuery2.sql
@@ -0,0 +1,30 @@
+﻿USE Master
+GO	
+IF DB_ID('BILER') IS NOT NULL
+	BEGIN
+ALTER DATABASE BILER SET SINGLE_USER
+DROP DATABASE BILER
+	END
+CREATE DATABASE BILER 
+GO
+USE BILER
+GO
+
+CREATE TABLE BIL (
+Maerke nvarchar (50),
+Farve nvarchar(50)
+)
+
+INSERT INTO BIL (Maerke,Farve)
+VALUES ('Mercedes','Grøn')
+
+INSERT INTO BIL (Maerke,Farve)
+VALUES ('BMW','Rød')
+
+INSERT INTO BIL (Maerke,Farve)
+VALUES ('Mazda','Hvid')
+
+INSERT INTO BIL (Maerke,Farve)
+VALUES ('Toyota','Blå')
+
+SELECT * FROM BIL
